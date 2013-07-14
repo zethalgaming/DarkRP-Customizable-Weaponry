c51a0884265395026ac83daa8fb37cf4522eb17b
ToXIcGaming <toxicgames@live.co.uk>
2013-07-14 13:07:51 +0100

    Added Add Entities File

---
+++ addentities.lua
@@ -0,0 +1,144 @@
+AddCustomShipment("Desert eagle", {
+	model = "models/weapons/w_pist_deagle.mdl",
+	entity = "cstm_pistol_deagle",
+	price = 215,
+	amount = 10,
+	seperate = true,
+	pricesep = 215,
+	noship = true,
+	allowed = {TEAM_GUN}
+})
+
+AddCustomShipment("Fiveseven", {
+	model = "models/weapons/w_pist_fiveseven.mdl",
+	entity = "cstm_pistol_fiveseven",
+	price = 0,
+	amount = 10,
+	seperate = true,
+	pricesep = 205,
+	noship = true,
+	allowed = {TEAM_GUN}
+})
+
+AddCustomShipment("P228", {
+	model = "models/weapons/w_pist_p228.mdl",
+	entity = "cstm_pistol_p228",
+	price = 0,
+	amount = 10,
+	seperate = true,
+	pricesep = 185,
+	noship = true,
+	allowed = {TEAM_GUN}
+})
+
+AddCustomShipment("Reflex Sight", {
+	model = "models/wystan/attachments/2octorrds.mdl",
+	entity = "upgr_reflexsight",
+	price = 0,
+	amount = 10,
+	seperate = true,
+	pricesep = 25,
+	noship = true,
+	allowed = {TEAM_GUN}
+})
+
+
+AddCustomShipment("Rifle Reflex", {
+	model = "models/attachments/kascope.mdl",
+	entity = "upgr_riflereflex",
+	price = 0,
+	amount = 10,
+	seperate = true,
+	pricesep = 25,
+	noship = true,
+	allowed = {TEAM_GUN}
+})
+
+AddCustomShipment("Vertical Grip", {
+	model = "models/wystan/attachments/foregrip1.mdl",
+	entity = "upgr_vertgrip",
+	price = 0,
+	amount = 10,
+	seperate = true,
+	pricesep = 25,
+	noship = true,
+	allowed = {TEAM_GUN}
+})
+
+AddCustomShipment("AK47", {
+	model = "models/weapons/w_rif_ak47.mdl",
+	entity = "cstm_rif_ak47",
+	price = 2450,
+	amount = 10,
+	seperate = false,
+	pricesep = nil,
+	noship = false,
+	allowed = {TEAM_GUN}
+})
+
+AddCustomShipment("Galil", {
+	model = "models/weapons/w_rif_galil.mdl",
+	entity = "cstm_rif_galil",
+	price = 2200,
+	amount = 10,
+	seperate = false,
+	pricesep = nil,
+	noship = false,
+	allowed = {TEAM_BLACK}
+})
+
+AddCustomShipment("MP5", {
+	model = "models/weapons/w_smg_mp5.mdl",
+	entity = "cstm_smg_mp5",
+	price = 2200,
+	amount = 10,
+	seperate = false,
+	pricesep = nil,
+	noship = false,
+	allowed = {TEAM_GUN}
+})
+
+AddCustomShipment("Famas", {
+	model = "models/weapons/w_rif_famas.mdl",
+	entity = "cstm_rif_famas",
+	price = 2450,
+	amount = 10,
+	seperate = false,
+	pricesep = nil,
+	noship = false,
+	allowed = {TEAM_GUN}
+})
+
+AddCustomShipment("Mac 10", {
+	model = "models/weapons/w_smg_mac10.mdl",
+	entity = "cstm_smg_mac10",
+	price = 2150,
+	amount = 10,
+	seperate = false,
+	pricesep = nil,
+	noship = false,
+	allowed = {TEAM_GUN}
+})
+
+AddCustomShipment("M3 Shotgun", {
+	model = "models/weapons/w_shot_m3super90.mdl",
+	entity = "cstm_shotgun_m3",
+	price = 1750,
+	amount = 10,
+	seperate = false,
+	pricesep = nil,
+	noship = false,
+	allowed = {TEAM_GUN}
+})
+
+AddCustomShipment("Sniper rifle", {
+	model = "models/weapons/w_snip_scout.mdl",
+	entity = "cstm_sniper_scout",
+	price = 3750,
+	amount = 10,
+	seperate = false,
+	pricesep = nil,
+	noship = false,
+	allowed = {TEAM_GUN}
+})
+
